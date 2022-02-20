2d6d502d782297caa8977a8f7c8c208aa55f2b63
Robert E. Novak <sailnfool@gmail.com>
2022-02-20 12:05:59 -0800

    Generated Results

---
+++ results/script.sh
@@ -0,0 +1,8 @@
+cpuspeed_function -n -s b2sum 1Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-02_20220220_19:54.csv
+cpuspeed_function -n -s sha1sum 1Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-02_20220220_19:54.csv
+cpuspeed_function -n -s sha256sum 1Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-02_20220220_19:54.csv
+cpuspeed_function -n -s sha512sum 1Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-02_20220220_19:54.csv
+cpuspeed_function -n -s b2sum 5Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-02_20220220_19:54.csv
+cpuspeed_function -n -s sha1sum 5Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-02_20220220_19:54.csv
+cpuspeed_function -n -s sha256sum 5Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-02_20220220_19:54.csv
+cpuspeed_function -n -s sha512sum 5Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-02_20220220_19:54.csv
