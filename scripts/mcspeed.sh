fa2f624f850561de4eaa9bf584ccdaef648beec3
Robert E. Novak <sailnfool@gmail.com>
2022-02-25 03:49:27 -0800

    Made a lot of changes to add both number of copies
    and the wait time after making the copies file.

---
+++ scripts/mcspeed.sh
@@ -0,0 +1,429 @@
+#!/bin/bash
+scriptname=${0##*/}
+########################################################################
+# Copyright (c) 2020 Sea2Cloud Storage, Inc.  All Rights Reserved
+# Modesto, CA 95356
+#
+# mcpuspeed - Collect relative performance data on CPU speed
+# Author - Robert E. Novak aka REN
+#	sailnfool@gmail.com
+#	skype:sailnfool.ren
+# License CC by Sea2Cloud Storage, Inc.
+# see https://creativecommons.org/licenses/by/4.0/legalcode
+# for a complete copy of the Creative Commons Attribution license
+#_____________________________________________________________________
+# Rev.|Auth.| Date     | Notes
+#_____________________________________________________________________
+# 1.3 | REN |02/25/2022| concatenate N copies of the dictionary file
+#                      | together before starting to iterate the
+#                      | cryptographic hashes over 1/Nth the number of
+#                      | iterations.
+# 1.2 | REN |02/20/2022| Track memory and swap usage as well as the
+#                      | Date and time run
+# 1.1 | REN |02/20/2022| Tweaked into a separate github repository
+#                      | Added tracking of memory and swap as well as
+#                      | a complete timestamp for each record created
+#                      | Note that the timestamp is in UCT
+# 1.0 | REN |02/01/2022| Initial Release
+#_____________________________________________________________________
+#
+########################################################################
+# This script was motivated by the fact that I could run Ubuntu on a
+# Raspberry PI 4 8GB (or 4GB) model.  The Linux Foundation edx script
+# which # attempts to decide if a machine has the capacity to run their 
+# courseware uses bogomips to determine the relative CPU speed of the
+# subject system uses bogomips.  You can look at Wikipedia for more
+# information on bogomips. The Linux Foundation script is
+# "ready-for.sh" and it contains an internal database of the estimated
+# performance levels necessary to run their courseware for various
+# courseware.  I was taking a rather elementary course which was
+# looking for a bogomips rating of 2000 as the minimum CPU performance
+# for the courseware.  Running on a Raspberry PI4 8GB model the
+# bogomips score came up with a value of 108, which would suggest that
+# the Raspberry PI was too slow to run the courseware by a factor of 
+# 20.  Since I was using these Raspberry PI machines for regular 
+# desktop software development, I felt that this characterization was
+# unrealistic for the Raspberry PI 4.  I have developed this script to
+# develop some alternative measures of the system performance and to
+# correlate this speed against existing bogomips scores to try to
+# generate a "more fair" estimate of system peformance, especially for
+# ARM CPUs (and emerging RISC V CPUS which I suspect will have the
+# same measurement challenge).
+#
+# Using cryptograhic hashes against a dictionary as a proxy for
+# bogospeed.  To give a variety of data, the driving script will invoke
+# this with different iteration counts that will push memory, core and
+# sample I/O rates.
+#
+# Note that this script also supports other cryptographic hash functions
+# commonly found on Linux systems, including:
+# sha1sum, sha256sum, sha512sum, b2sum
+#
+#_____________________________________________________________________
+# Rev.|Auth.| Date     | Notes
+#_____________________________________________________________________
+#
+source func.nice2num
+source func.errecho
+source func.insufficient
+source func.toseconds
+source func.os
+
+########################################################################
+# Set the # default values that can be overridden on the command lines.
+########################################################################
+numcopies=1
+NUMARGS=1
+FUNC_DEBUG="0"
+verbosemode="FALSE"
+export FUNC_DEBUG
+dictpath=/usr/share/dict/
+language=american-english
+OUTFILE=/tmp/timer_out$$.txt
+hashprogram=$(which sha256sum)
+timerfailure="FALSE"
+waitdivisor=8
+
+USAGE="\r\n${scriptname} [-h] [-c <#>] [-l <language> ] [-n]\r\n
+\t\t\t[-s <hashprogram>] [-w <#>] <nicenumber>\r\n
+\t\tProvide a nicenumber to control the iterations of the number of\r\n
+\t\ttimes that we take the sha256sum of the local dictionary.  This\t\n
+\t\twill report the architecture of the machine, the size of the\r\n
+\t\tdictionary, the number of iterations, bogomips, time per hashing\r\n
+\t\t10Mib characters as normalization\r\n
+\t-c\t<#>\tthe number of copies of the dictionary that are\r\n
+\t\t\tconcatenated together to diminish the open/close processing\r\n
+\t\t\tfor the opening and closing of the input file. The default\r\n
+\t\t\tnumber of copies is ${numcopies}.  Suggested values are\r\n
+\t\t\tbetween 128 and 512 copies.  See also -w below\r\n
+\t-h\tPrint this message\r\n
+\t-l\t<language>\tthe name of an alternate dictionary\r\n
+\t-n\tSend the hash output to /dev/null\r\n
+\t-s\t<hashprogram>\tSpecify which cryptographic hash\r\n
+\t\t\tprogram to use valid values:\r\n
+\t\t\tb2sum; sha1sum; sha256sum; sha512sum\r\n
+\t-v\t\tturn on verbose mode, currently ignored\r\n
+\t-w\t<#>\tthe divisor used to provide a \"wait\" time for the \r\n
+\t\t\tcopies operation to complete.  When making (e.g. 512) copies\r\n
+\t\t\tof dictionary that is ~1MB in size, it takes time for that \r\n
+\t\t\toperation to complete so that it does not delay the \"timing\"\r\n
+\t\t\tfunction.  Using the graphical gnome tools that display the\r\n
+\t\t\twrite operations for the system, I have experimented with this\r\n
+\t\t\ton a Raspberry PI 4 and it appears that it takes a divisor value\r\n
+\t\t\tof between 8 and 20.   Your mileage may vary so perform your own\r\n
+\t\t\ttesting.
+\t<nicenumber>\tA number in the format \"1M\" or \"1Mib\" to\r\n
+\t\t\trepresent 1 M (Mbyte) ${__kbytesvalue["M"]}\r\n
+\t\t\tor 1 Mib (Mbibyte) ${__kbibytesvalue["MIB"]}\r\n
+\t\t\tor 1 Pib (Pbibyte) ${__kbibytesvalue["PIB"]}\r\n
+\t\t\tor other bibyte prefix (e.g., eta or zeta)\r\n
+\r\n
+\t\tFor a complete table of nice suffixes and values\r\n
+\t\t${scriptname} -v -h\r\n
+"
+
+########################################################################
+# Define all of the optionargs documented in USAGE.
+########################################################################
+optionargs="c:hl:ns:vw:"
+
+########################################################################
+# Process the Command Line options based on the Usage above
+########################################################################
+while getopts ${optionargs} name
+do
+  case ${name} in
+  c)
+    if [[ "${OPTARG}" =~ $re_integer ]]
+    then
+      numcopies="${OPTARG}"
+    else
+      errorecho -e "-c requires an integer argument"
+      errorecho -e ${USAGE}
+      exit 1
+    fi
+    ;;
+  h)
+    echo -e ${USAGE}
+    if [[ "${verbosemode}" == "TRUE" ]]
+    then
+      for i in $(seq 0 $((${#__kbytessuffix}-1)) )
+      do
+        bytesuffix=${__kbytessuffix:${i}:1}
+        echo -e "${bytesuffix}\t${__kbytesvalue[${bytesuffix}]}"
+      done
+      echo ""
+      for i in $(seq 0 $((${#__kbytessuffix}-1)) )
+      do
+        bibytesuffix=${__kbibytessuffix[${i}]}
+        echo -e "${bibytesuffix}\t${__kbibytesvalue[${bibytesuffix}]}"
+      done
+    fi
+    exit 0
+    ;;
+  l)
+    language="${OPTARG}"
+    ;;
+  s)
+    hashprogram="${OPTARG}"
+    case ${hashprogram} in
+      b2sum)
+        hashprogram=$(which ${hashprogram})
+        hashprogram=${hashprogram##*/}
+        ;;
+      sha1sum)
+        hashprogram=$(which ${hashprogram})
+        hashprogram=${hashprogram##*/}
+        ;;
+      sha256sum)
+        hashprogram=$(which ${hashprogram})
+        hashprogram=${hashprogram##*/}
+        ;;
+      sha512sum)
+        hashprogram=$(which ${hashprogram})
+        hashprogram=${hashprogram##*/}
+        ;;
+      \?)
+        errecho "-e" "Invalid hash program: ${OPTARG}"
+        errecho "-e" ${USAGE}
+        exit 2
+    esac
+    ;;
+  n)
+    OUTFILE="/dev/null"
+    ;;
+  v)
+    verbosemode="TRUE"
+    ;;
+  w)
+    if [[ "${OPTARG}" =~ $re_integer ]]
+    then
+      waitdivisor="${OPTARG}"
+    else
+      errecho -e "-w require an integer argument"
+      errecho -e ${USAGE}
+      exit 1
+    fi
+    ;;
+  \?)
+    errecho "-e" "invalid option: ${OPTARG}"
+    errecho "-e" ${USAGE}
+    exit 1
+    ;;
+  esac
+done
+shift $((OPTIND-1))
+########################################################################
+# End of processing Command Line arguments
+########################################################################
+
+
+if [[ $# -lt ${NUMARGS} ]]
+then
+  errecho "-e" ${USAGE} $@
+  insufficient ${NUMARGS} $@
+  exit -2
+else
+  POWER=$1
+fi
+
+########################################################################
+# Convert the nice notation to a number
+########################################################################
+iterations=$(nice2num "${POWER}")
+iterations=$((iterations / ${numcopies}))
+
+########################################################################
+# Create a shell script in /tmp which we will execute the test
+# programs.
+# the application script under test that invokes the test programs
+# is referred to as TIMER_APP.
+# The output of /usr/bin/time is captured in TIMER_OUT from which the
+# Elapsed, User and System time spent in the TIMER_APP is extracted.
+# The input file to the the test program is placed in TIMER_INPUT.
+# Nominally this is the default language file in the dictionary path
+# on this machine.  For American English, this is:
+# /usr/share/dict/american-english.  Since this file can vary 
+# according to the locale, the absolute times are normalized by 
+# dividing the number of total bytes hashed by 1,000,000
+########################################################################
+TIMER_APP_DEV_NULL=/tmp/mctimer_null$$.sh
+TIMER_APP=/tmp/mctimer$$.sh
+TIMER_OUT=/tmp/mctimeout$$.txt
+TIMER_INPUT=/tmp/mctimein$$
+rm -f ${TIMER_APP} ${TIMER_OUT} ${TIMER_INPUT}
+
+########################################################################
+# In order to minimize the overhead for each test, if the copies of the
+# dictionary have already been done, then just link the input file to
+# that number of copies.  Otherwise we make the number of copies of the
+# dictionary in /tmp and link that to the TIMER_INPUT
+########################################################################
+if [[ -r /tmp/${language}_${numcopies} ]]
+then
+  ln /tmp/${language}_${numcopies} ${TIMER_INPUT}
+else
+  for i in $(seq ${numcopies})
+  do 
+    cat ${dictpath}/${language} >> /tmp/${language}_${numcopies}
+  done
+  echo "Sleeping for $((numcopies/waitdivisor)) seconds to allow copy to complete"
+  sleep $((numcopies/waitdivisor)) # Observed behavior is that the first time the file is 
+          # created it impacts the timing of the program under test
+          # for large files, presumably waiting for this write
+          # operation to finish. The value of 5 seconds is arbitrary
+          # further testing should be done to make this wait time be
+          # a function of the numcopies.  The formula just added
+          # assumes 5 seconds for 100 copies and scales from there
+  ln /tmp/${language}_${numcopies} ${TIMER_INPUT}
+fi
+
+########################################################################
+# If the OUTFILE is /dev/null we can eliminate the extra test steps
+# we create two TIMER_APPs, one that handles the case that the output
+# is NOT /dev/null and one that assumes that the output is sent to
+# /dev/null and does less work.  This latter is most likely the one
+# that will be used.
+########################################################################
+cat > ${TIMER_APP} <<EOF
+#!/bin/bash
+rm -f ${OUTFILE}
+i=0
+while [[ \${i} -lt ${iterations} ]]
+do
+  ${hashprogram} ${TIMER_INPUT} >> ${OUTFILE}
+  i=\$((i+1))
+done
+rm -f ${OUTFILE}
+exit 0
+EOF
+cat > ${TIMER_APP_DEV_NULL} <<EOFNULL
+#!/bin/bash
+i=0
+while [[ \${i} -lt ${iterations} ]]
+do
+  ${hashprogram} ${TIMER_INPUT} >> ${OUTFILE}
+  i=\$((i+1))
+done
+exit 0
+EOFNULL
+if [[ "${OUTFILE}" = "/dev/null" ]]
+then
+  mv ${TIMER_APP_DEV_NULL} ${TIMER_APP}
+else
+  rm -f ${TIMER_APP_DEV_NULL}
+fi
+
+########################################################################
+# Make sure that the app we will be timing is executable and then
+# run it, capturing the elapsed, user and system time with
+# /usr/bin/time
+########################################################################
+chmod +x ${TIMER_APP}
+/usr/bin/time ${TIMER_APP} 2> ${TIMER_OUT}
+timeresult=$?
+
+########################################################################
+# At this point the test is completed.  Now we gather all of the 
+# statistics and emit them to the results file.  If the timing failed
+# then note that failure and modify the output line accordingly.
+########################################################################
+if [[ "${timeresult}" -ne 0 ]]
+then
+  # echo "Failure from /usr/bin/time ${TIMER_APP}."
+  # echo "output in ${TIMER_OUT}"
+  timerfailure="TRUE"
+  # exit 1
+fi
+usertime=$(cat ${TIMER_OUT} | \
+  sed -e '2d' -e '1,1s/^\([0-9:\.][0-9:\.]*\)user.*$/\1/')
+userseconds=$(toseconds ${usertime})
+systemtime=$(cat ${TIMER_OUT} | \
+  sed -e '2d' -e '1,1s/^.*user \([0-9:\.][0-9:\.]*\)system.*$/\1/') 
+systemseconds=$(toseconds ${systemtime})
+elapsedtime=$(cat ${TIMER_OUT} | \
+  sed -e '2d' -e '1,1s/^.*system \([0-9:\.][0-9:\.]*\)elapsed.*$/\1/')
+elapsedseconds=$(toseconds ${elapsedtime})
+
+########################################################################
+# Collect the rest of the system data
+########################################################################
+
+########################################################################
+# Remembering if the timing was a failure, add the word "FAILURE - " to
+# the name of the system architecture so we know we need to rerun this
+# test combination.
+########################################################################
+cat ${TIMER_OUT}
+if [[ "${timerfailure}" == "TRUE" ]]
+then
+  rm -f ${TIMER_APP} ${TIMER_OUT}
+  Arch="FAILURE - $(uname -m)"
+else
+  Arch=$(uname -m)
+fi
+
+########################################################################
+# Find out the maximum frequency at which the processors run.  Although
+# Transmeta failed as a CPU company, their legacy lives on by lowering
+# the clock speed of cores that are essentially idle (hence part of
+# the fallacy of using Bogomips as a performance metric).
+########################################################################
+maxCPU=$(lscpu | grep  -i "CPU max MHz" | \
+  sed "s/^.*:[^0-9]*\([0-9\.][0-9\.]*\)/\1/")
+cores=$(lscpu | grep -i "^CPU(s)" |
+  sed "s/^.*:[^0-9]*\([0-9\.][0-9\.]*\)/\1/")
+memtotal=$(free -h | awk '/^Mem:/ {print $2}')
+memused=$(free -h | awk '/^Mem:/ {print $3}')
+memfree=$(free -h | awk '/^Mem:/ {print $4}')
+swaptotal=$(free -h | awk '/^Swap:/ {print $2}')
+swapused=$(free -h | awk '/^Swap:/ {print $3}')
+swapfree=$(free -h | awk '/^Swap:/ {print $4}')
+bogomips=$(lscpu | grep -i "bogomips" | \
+  sed "s/^.*:[^0-9]*\([0-9\.][0-9\.]*\)/\1/")
+
+########################################################################
+# I know that it seems odd to always use UCT time rather than local
+# time, but if the testing is done on machines in different time
+# zones this will keep the testing times straight
+########################################################################
+UCTdatetime=$(date -u "+%Y%m%d_%H%M")
+dictsize=$(stat --printf="%s" /usr/share/dict/${language})
+
+########################################################################
+# Note that in computing the totsize that we multiply by the number
+# of copies so that the rate data is not skewed.
+########################################################################
+totsize=$(echo "${dictsize} * ${iterations} * ${numcopies}" | bc)
+elapsedrate=$(echo "( ${totsize} / ${elapsedseconds} ) / 1000000" | bc)
+userrate=$(echo "( ${totsize} / ${userseconds} ) / 1000000" | bc)
+systemrate=$(echo "( ${totsize} / ${systemseconds} ) / 1000000" | bc)
+
+########################################################################
+rm -f ${TIMER_APP} ${TIMER_OUT} ${TIMER_INPUT}
+
+########################################################################
+# We emit the header line every time.  Although mildly redundant this
+# can be elided either by a spreadsheet application or by using 
+# sort -u to eiliminate the redundant copies
+########################################################################
+echo -n "Architecture|cores|CPU max MHz|"
+echo -n "MEM Total|MEM Used|MEM Free|"
+echo -n "SWAP Total|SWAP Used|SWAP Free|"
+echo -n "OS|Bogomips|UCT Date|"
+echo -n "Dictionary bytes|Iterations|"
+echo -n "Cryptographic Hash|WallTime|UserTime|Systime|"
+echo -n "MB/Sec Wall|MB/Sec User|MB/Sec System"
+echo ""
+
+########################################################################
+# Emit the actual data for this test.
+########################################################################
+echo -n "${Arch}|${cores}|${maxCPU}|"
+echo -n "${memtotal}|${memused}|${memfree}|"
+echo -n "${swaptotal}|${swapused}|${swapfree}|"
+echo -n "$(func_os)|${bogomips}|${UCTdatetime}|"
+echo -n "${dictsize}|${iterations}|${hashprogram}|"
+echo -n "${elapsedseconds}|${userseconds}|${systemseconds}|"
+echo -n "${elapsedrate}|${userrate}|${systemrate}"
+echo ""
---
+++ scripts/mwrapper.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+scriptname=${0##*/}
+########################################################################
+# Copyright (c) 2020 Sea2Cloud Storage, Inc.  All Rights Reserved
+# Modesto, CA 95356
+#
+# wrapper_cpuspeed - generate a script that will repeatedly invoke a 
+#                    list of hash functions with different repetition
+#                    counts for the cpuspeed_function
+# Author - Robert E. Novak aka REN
+#	sailnfool@gmail.com
+#	skype:sailnfool.ren
+# License CC by Sea2Cloud Storage, Inc.
+# see https://creativecommons.org/licenses/by/4.0/legalcode
+# for a complete copy of the Creative Commons Attribution license
+#_____________________________________________________________________
+# Rev.|Auth.| Date     | Notes
+#_____________________________________________________________________
+# 1.1 | REN |02/24/2022| Added a parameter to override the date stamp
+#                      | placed on the results csv file.
+# 1.0 | REN |02/20/2022| Initial Release
+#_____________________________________________________________________
+########################################################################
+source func.kbytes
+source func.nice2num
+source func.errecho
+source func.insufficient
+source func.regex
+
+########################################################################
+# Declare the list of default hash functions that will be tested
+########################################################################
+declare -a hashes
+hashes=("b2sum" "sha1sum" "sha256sum" "sha512sum")
+
+########################################################################
+# Verify that binaries for each of the hash functions are found on 
+# this machine
+########################################################################
+for myhash in "${!hashes[@]}"
+do
+  if [[ ! $(which ${hashes[${myhash}]}) ]]
+  then
+    errecho "-e" "Cryptographic Hash Function ${hashes[${myhash}]} not found"
+    errecho "-e" "Have you installed the \"coreutils\" package?"
+    sudo apt install coreutils
+  fi
+done
+if [[ ! $(which time) ]]
+then
+  errecho "-e" "/usr/bin/time not found"
+  errecho "-e" "Have you installed the time package?"
+  sudo apt install time
+fi
+
+UCTdatetime=$(date -u "+%Y%m%d_%H%M")
+testname="$(hostname)_${UCTdatetime}"
+maxiterations=50
+iterincrement=10
+numcopies=512
+waitdivisor=8
+suffix=${UCTdatetime}
+########################################################################
+# Process the command line options
+########################################################################
+USAGE="${0##*/} [-h] [-c <#>] [-d <suffix>] [-f <testname>i] [<maxiterations> [<increment>]]\r\n
+\t\t<maxiterations> (default ${maxiterations})is the number\r\n
+\t\tof times that the test is performed for each hashtype.  Note\r\n
+\t\tis multiplied by 1024\r\n
+\t\t<increment> (default ${iterincrement}) is the number of\r\n
+\t\tof iterations to bump the test count by each time\t\n
+\t-c\t<#>\tThe number of copies of the input file to concatenate\r\n
+\t\t\ttogether to minimize the open/close overhead.\r\n
+\t-d\t<suffix>\tis the string just prior to the ".csv" extension that
+\t\t\tcan uniquely identify the generate script.  The default is
+\t\t\tnormally the current date time (e.g. ${UCTdatetime})
+\t-f\t<testname> (default ${testname}) is the name of the CSV\r\n
+\t\t\tfile where the results are tabulated\r\n
+\t-h\t\tOutput this message\r\n
+"
+
+optionargs="c:d:hf:"
+NUMARGS=0 #No arguments are mandatory
+
+while getopts ${optionargs} name
+do
+  case ${name} in
+  c)
+    if [[ "${OPTARG}" =~ ${re_integer} ]]
+    then
+      numcopies="${OPTARG}"
+    else
+      errecho -e "-c requires an integer argument"
+      errecho -e ${USAGE}
+      exit 1
+    fi
+    ;;
+  d)
+    suffix="${OPTARG}"
+    testname="$(hostname)_${suffix}"
+    ;;
+  h)
+    echo -e ${USAGE}
+    exit 0
+    ;;
+  f)
+    testname="${OPTARG}"
+    ;;
+  \?)
+    errecho "-e" "invalid option: ${OPTARG}"
+    errecho "-e" ${USAGE}
+    exit 1
+    ;;
+  esac
+done
+shift $((OPTIND-1))
+
+########################################################################
+# Even though the current incarnation has no required arguments, this
+# code will save time later if we have required arguments
+########################################################################
+if [[ "$#" -lt ${NUMARGS} ]]
+then
+  errecho "-e" ${USAGE} $@
+  insufficient ${NUMARGS} $@
+  exit -2
+fi
+
+########################################################################
+# Process the optional argument max iterations which is the number of
+# iterations of the dictionary that will be hashed.  The iteration
+# number is multiplied by 1024
+########################################################################
+if [[ "$#" -ge "1" ]]
+then
+  if [[ "$1" =~ $re_integer ]]
+  then
+    maxiterations=$1
+  else
+    errecho "-e" "<maxiterations> must be an integer"
+    errecho "-e" ${USAGE}
+    exit 1
+  fi
+  shift
+fi
+
+########################################################################
+# Process the increment between interations trials.  Note that we start
+# increments at 1, the second increment is increment - 1, so if
+# increment were 10, the sequence would be 1, 10, 20...  If it were 5
+# the sequence would be 1, 5, 10, 15....
+########################################################################
+if [[ "$#" -ge "1" ]]
+then
+  if [[ "$1" =~ $re_integer ]]
+  then
+    iterincrement=$1
+  else
+    errecho "-e" "<iterincrement> must be an integer"
+    errecho "-e" ${USAGE}
+    exit 1
+  fi
+  shift
+fi
+
+########################################################################
+# We want to keep the results in the repository
+########################################################################
+outdir=$HOME/github/cpuspeed/results
+testoutput=${testname}.csv
+scripter=script_${testname}.sh
+outfile=${outdir}/${testoutput}
+max=${maxiterations}
+
+########################################################################
+# This is where we generate the bash command lines to invoke the test 
+# function a number of times against a dictionary.  Note that we not
+# only generate a given number of iterations, but we repeat that for
+# each type of cryptographic hash that we know about.
+# The output of this command is suitable for piping into bash or for
+# placing in a file to be executed later.
+########################################################################
+rm -f ${outdir}/${scripter} ${outfile} ${outdir}/sorted.${testoutput}
+echo "#!/bin/bash" | tee -a ${outdir}/${scripter}
+count=1
+while [[ ${count} -le ${max} ]]
+do
+  for myhash in "${!hashes[@]}"
+  do
+    echo "mcspeed -c ${numcopies} -w ${waitdivisor} -n -s ${hashes[${myhash}]} ${count}Kib |" \
+      "tee -a ${outdir}/${testoutput}" | tee -a ${outdir}/${scripter}
+  done
+  if [[ "${count}" -eq "1" ]]
+  then
+    count=$((count+(iterincrement-1)))
+  else
+    count=$((count+iterincrement))
+  fi
+done
+exit 0
