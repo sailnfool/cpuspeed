3574f3211647556d425b59b3aed171cfcbd3fc09
Robert E. Novak <sailnfool@gmail.com>
2022-02-25 06:46:20 -0800

    Initial

---
+++ results/oldscripts/script_ldap.sea2cloud.com_dd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ddspeed -n -s dd 1Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 5Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 10Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 15Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 20Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 25Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 30Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 35Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 40Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 45Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 50Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 55Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 60Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 65Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 70Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 75Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 80Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 85Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 90Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 95Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
+ddspeed -n -s dd 100Kib | tee -a /home/rnovak/github/cpuspeed/results/ldap.sea2cloud.com_dd.csv
