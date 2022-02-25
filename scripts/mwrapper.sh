e812b87dc353ec159b3d4c05c6939b8178c397d4
Robert E. Novak <sailnfool@gmail.com>
2022-02-25 06:45:59 -0800

    Fixed documentation in -h and added -n and -w options

--- scripts/mwrapper.sh
+++ scripts/mwrapper.sh
@@ -4,9 +4,12 @@ scriptname=${0##*/}
 # Copyright (c) 2020 Sea2Cloud Storage, Inc.  All Rights Reserved
 # Modesto, CA 95356
 #
-# wrapper_cpuspeed - generate a script that will repeatedly invoke a 
-#                    list of hash functions with different repetition
-#                    counts for the cpuspeed_function
+# mwrapper - generate a script that will repeatedly invoke a
+#            list of hash functions with different repetition
+#            counts for the mcspeed script which includes not
+#            only hash functions but can invoke dd to look at
+#            subtracting the I/O overhead from the hash
+#            computation.
 # Author - Robert E. Novak aka REN
 #	sailnfool@gmail.com
 #	skype:sailnfool.ren
@@ -63,7 +66,8 @@ suffix=${UCTdatetime}
 ########################################################################
 # Process the command line options
 ########################################################################
-USAGE="${0##*/} [-h] [-c <#>] [-d <suffix>] [-f <testname>i] [<maxiterations> [<increment>]]\r\n
+USAGE="${0##*/} [-h] [-c <#>] [-w <#>] [-d <suffix>] [-f <testname>i] [-n]\r\n
+\t\t\t[<maxiterations> [<increment>]]\r\n
 \t\t<maxiterations> (default ${maxiterations})is the number\r\n
 \t\tof times that the test is performed for each hashtype.  Note\r\n
 \t\tis multiplied by 1024\r\n
@@ -71,15 +75,28 @@ USAGE="${0##*/} [-h] [-c <#>] [-d <suffix>] [-f <testname>i] [<maxiterations> [<
 \t\tof iterations to bump the test count by each time\t\n
 \t-c\t<#>\tThe number of copies of the input file to concatenate\r\n
 \t\t\ttogether to minimize the open/close overhead.\r\n
-\t-d\t<suffix>\tis the string just prior to the ".csv" extension that
-\t\t\tcan uniquely identify the generate script.  The default is
-\t\t\tnormally the current date time (e.g. ${UCTdatetime})
-\t-f\t<testname> (default ${testname}) is the name of the CSV\r\n
-\t\t\tfile where the results are tabulated\r\n
+\t-d\t<suffix>\tis the string just prior to the ".csv" extension\r\n
+\t\t\tthat can uniquely identify the generate script.  The \r\n
+\t\t\tdefault is normally the current date time \r\n
+\t\t\t(e.g. ${UCTdatetime})\r\n
+\t-f\t<testname> (default ${testname}) is \r\n
+\t\t\tthe name of the CSV file where the results are\r\n
+\t\t\ttabulated.\r\n
 \t-h\t\tOutput this message\r\n
+\t-n\t\tSend the hash output to /dev/null\r\n
+\t-w\t<#>\tthe divisor used to provide a \"wait\" time for the \r\n
+\t\t\tcopies operation to complete.  When making (e.g. 512)\r\n
+\t\t\tcopies tof dictionary that is ~1MB in size, it takes\r\n
+\t\t\ttime for that operation to complete so that it does\r\n
+\t\t\tnot delay the \"timing\" function.  Using the\r\n
+\t\t\tgraphical gnome tools that display the\r\n
+\t\t\twrite operations for the system, I have experimented\r\n
+\t\t\twith this on a Raspberry PI 4 and it appears that it\r\n
+\t\t\ttakes a divisor value of between 8 and 20.   Your\r\n
+\t\t\tmileage may vary so perform your own testing.\r\n
 "
 
-optionargs="c:d:hf:"
+optionargs="c:d:hf:nw:"
 NUMARGS=0 #No arguments are mandatory
 
 while getopts ${optionargs} name
@@ -105,6 +122,19 @@ do
     ;;
   f)
     testname="${OPTARG}"
+    ;;
+  n)
+    OUTFILE="/dev/null"
+    ;;
+  w)
+    if [[ "${OPTARG}" =~ $re_integer ]]
+    then
+      waitdivisor="${OPTARG}"
+    else
+      errecho -e "-w require an integer argument"
+      errecho -e ${USAGE}
+      exit 1
+    fi
     ;;
   \?)
     errecho "-e" "invalid option: ${OPTARG}"
