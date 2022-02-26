bf3c9901975d29fffc8f85c42dbe06d09d48b255
Robert E. Novak <sailnfool@gmail.com>
2022-02-26 12:44:18 -0800

    Initial

---
+++ results/working_scripts/script_PI04-08-03_dd1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mcspeed -c 1 -w 8 -n -s dd 1Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-03_dd1.csv
+mcspeed -c 1 -w 8 -n -s dd 5Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-03_dd1.csv
+mcspeed -c 1 -w 8 -n -s dd 10Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-03_dd1.csv
+mcspeed -c 1 -w 8 -n -s dd 15Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-03_dd1.csv
+mcspeed -c 1 -w 8 -n -s dd 20Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-03_dd1.csv
+mcspeed -c 1 -w 8 -n -s dd 25Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-03_dd1.csv
+mcspeed -c 1 -w 8 -n -s dd 30Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-03_dd1.csv
+mcspeed -c 1 -w 8 -n -s dd 35Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-03_dd1.csv
+mcspeed -c 1 -w 8 -n -s dd 40Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-03_dd1.csv
+mcspeed -c 1 -w 8 -n -s dd 45Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-03_dd1.csv
+mcspeed -c 1 -w 8 -n -s dd 50Kib | tee -a /home/rnovak/github/cpuspeed/results/PI04-08-03_dd1.csv
