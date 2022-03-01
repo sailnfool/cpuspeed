43c2a60c03e31208d108832290cdb5e099cc0899
Robert E. Novak <sailnfool@gmail.com>
2022-03-01 04:41:21 -0800

    Initial

---
+++ results/working_scripts/script_PI04-04-02_sha256sum_512.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha256sum 1Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha256sum 5Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha256sum 10Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha256sum 15Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha256sum 20Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha256sum 25Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha256sum 30Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha256sum 35Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha256sum 40Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha256sum 45Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha256sum 50Kib 
