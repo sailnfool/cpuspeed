738a15a95da100c579fc618a9b84d406d120700f
Robert E. Novak <sailnfool@gmail.com>
2022-03-01 04:51:38 -0800

    Initial

---
+++ results/working_scripts/script_pi3_sha1sum_512.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha1sum 1Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha1sum 5Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha1sum 10Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha1sum 15Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha1sum 20Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha1sum 25Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha1sum 30Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha1sum 35Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha1sum 40Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha1sum 45Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha1sum 50Kib 
