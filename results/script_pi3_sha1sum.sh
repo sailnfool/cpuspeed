56b80d2f07a14ced046c18de380df0f483b70b22
Robert E. Novak <sailnfool@gmail.com>
2022-03-03 23:46:53 -0800

    Initial

---
+++ results/script_pi3_sha1sum.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# /home/rnovak/github/cpuspeed/results/script_pi3_sha1sum.sh
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha1sum 1Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha1sum 5Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha1sum 10Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha1sum 15Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s sha1sum 1Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s sha1sum 5Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s sha1sum 10Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s sha1sum 15Kib 
