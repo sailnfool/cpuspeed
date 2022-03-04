47399bc0087d6d0d565c1d3310f418aa6b7735d1
Robert E. Novak <sailnfool@gmail.com>
2022-03-04 03:04:05 -0800

    DB Updated

---
+++ results/script_hplap_b2sum.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# /home/rnovak/github/cpuspeed/results/script_hplap_b2sum.sh
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s b2sum 1Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s b2sum 5Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s b2sum 10Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s b2sum 15Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s b2sum 1Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s b2sum 5Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s b2sum 10Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s b2sum 15Kib 
---
+++ results/script_hplap_sha256sum.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# /home/rnovak/github/cpuspeed/results/script_hplap_sha256sum.sh
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha256sum 1Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha256sum 5Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha256sum 10Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha256sum 15Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s sha256sum 1Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s sha256sum 5Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s sha256sum 10Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s sha256sum 15Kib 
