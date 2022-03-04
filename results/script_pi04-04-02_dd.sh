fc7ef55e48a828819f78b98490ab114805c978b0
Robert E. Novak <sailnfool@gmail.com>
2022-03-04 03:06:33 -0800

    DB Updated

---
+++ results/script_pi04-04-02_dd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# /home/rnovak/github/cpuspeed/results/script_pi04-04-02_dd.sh
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s dd 1Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s dd 5Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s dd 10Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s dd 15Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s dd 1Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s dd 5Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s dd 10Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s dd 15Kib 
---
+++ results/script_pi04-04-02_sha512sum.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# /home/rnovak/github/cpuspeed/results/script_pi04-04-02_sha512sum.sh
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha512sum 1Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha512sum 5Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha512sum 10Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 512       -w 8 -n -s sha512sum 15Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s sha512sum 1Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s sha512sum 5Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s sha512sum 10Kib 
+mcspeed -r "/home/rnovak/github/cpuspeed/results" -c 1       -w 8 -n -s sha512sum 15Kib 
