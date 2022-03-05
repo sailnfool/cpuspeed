a6be2f556b929d27569ee6e37bc7a096f4b1a777
Robert E. Novak <sailnfool@gmail.com>
2022-03-04 18:36:24 -0800

    Adding comments and generalizing a cross platform test...
    process

--- scripts/dohosts.sh
+++ scripts/dohosts.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
+scriptname=${0##*/}
+########################################################################
+# Copyright (c) 2022 Sea2Cloud Storage, Inc.  All Rights Reserved
+# Modesto, CA 95356
+#
+# dohosts - for a set of hosts, run the complete set of performance
+#           tests based on cryptographic hashes.
+# Author - Robert E. Novak aka REN
+#	sailnfool@gmail.com
+#	skype:sailnfool.ren
+# License CC by Sea2Cloud Storage, Inc.
+# see https://creativecommons.org/licenses/by/4.0/legalcode
+# for a complete copy of the Creative Commons Attribution license
+#_____________________________________________________________________
+# Rev.|Auth.| Date     | Notes
+#_____________________________________________________________________
+# 1.0 | REN |03/04/2022| Initial Release
+#_____________________________________________________________________
+#
 scriptfile=/tmp/doscripts_$$.sh
 cat > ${scriptfile} << EOF
 #!/bin/bash
+if [[ ! -d /home/rnovak/github/mcperf ]]
 cd ~rnovak/github/cpuspeed
 git pull
 sleep 3
