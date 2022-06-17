61290612d74bf8424cbf0a99e039903067c978d5
Robert E. Novak <sailnfool@gmail.com>
2022-06-16 23:14:25 -0700

    Count only the atkin processses

--- tests/multitester.atkin.sh
+++ tests/multitester.atkin.sh
@@ -7,7 +7,7 @@ fi
 rm -f test.txt
 for i in $(seq 1 30)
 do
-  echo -e "$i\tcopies of atkins - $(ps -ax | grep atkin | wc -l)" \
+  echo -e "$i\tatkin copies - $(ps -ax | grep 'atkin ' | wc -l)" \
     "\t$(date)\r\n$(swapon --show)"|tee -a  test.txt
   ./tester.atkin.sh 30 40 &
   sync
