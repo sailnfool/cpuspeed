716b2de593b1945942f5b01bc605d3e35d9e7b33
Robert E. Novak <sailnfool@gmail.com>
2022-06-16 23:49:52 -0700

    Output to failoutput and used inxi for mem and swap info

--- src/multitester.atkin.sh
+++ src/multitester.atkin.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-cd ~/github/sysperf/tests
-if [[ -r test.txt ]]
+outfile="failoutput.txt"
+sleeptime=5
+cd ~/github/sysperf/src
+if [[ -r ${outfile} ]]
 then
-  cat test.txt
+  cat ${outfile}
 fi
-rm -f test.txt
+rm -f ${outfile}
+sudo inxi -Fz > ${outfile}
 for i in $(seq 1 30)
 do
   echo -e "$i\tatkin copies - $(ps -ax | grep 'atkin ' | wc -l)" \
-    "\t$(date)\r\n$(swapon --show)"|tee -a  test.txt
+    "\t$(date)\r\n$(inxi -I --swap)"|tee -a  ${outfile}
   ./tester.atkin.sh 30 40 &
   sync
   sync
-  sleep 10
+  sleep ${sleeptime}
 done
